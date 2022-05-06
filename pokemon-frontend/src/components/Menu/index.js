62d31e25f1da00c58a7ea98788945437ce94acc6
Leticiacb1 <leticiacb1@al.insper.edu.br>
2022-05-06 10:49:23 -0300

    Subindo info jogador e posicionamento de mapas.

--- pokemon-frontend/src/components/Menu/index.js
+++ pokemon-frontend/src/components/Menu/index.js
@@ -6,7 +6,6 @@ import logo from "../../logo.png";
 function Menu(){
 
     return (
-    
         <div className="screenGame">
             <img className="logo" src={logo} alt="logo PokeWay"/> 
             <div id='stars'></div>
@@ -18,9 +17,41 @@ function Menu(){
 
                     <div className="cardJogador">
 
+                        <div className="infoPlayer">
+                            <div className="avatar">
+                                <img src="https://i.dstatic.com/images/sprites/nuser255.png" alt="avatar" className="img_avatar"/>
+                            </div>
+                            <div className="infos">
+                                <h2 className="user_name">Username or email</h2>
+                            </div>
+                        </div>
+                        
+                        <div className="pokemons">
+                            <button className="botaoPokemons">
+                                See your Pokemons
+                            </button>
+                        </div>
+
+                        <div className="botaoLogout">
+                            <button className="botaoOut" >Log out</button>
+                        </div>
+
                     </div>
 
                     <div className="cardGame">
+                        
+                        <div className="containerMaps">
+                            <h2 className="opMap">Choose a map for play</h2>
+                            <div className="maps">
+                                <div className="map"></div>
+                                <div className="map"></div>
+                                <div className="map"></div>
+                            </div>
+                        </div>
+
+                        <div className= "botaoPlay">
+                            <button className="play">Play</button>
+                        </div>
 
                     </div>
 
