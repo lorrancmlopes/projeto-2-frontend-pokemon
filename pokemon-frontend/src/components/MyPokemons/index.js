59703d916212b434a25887792780760558916490
Lorran Caetano Machado Lopes <lorrancml@al.insper.edu.br>
2022-05-10 08:17:18 -0300

    altera formato do conteiner All Caught e add botão voltar

--- pokemon-frontend/src/components/MyPokemons/index.js
+++ pokemon-frontend/src/components/MyPokemons/index.js
@@ -28,7 +28,8 @@ function MyPokemons(){
             <div className="container">
                 <div className="allCaught">
                     <div className="allCaughtHeader">
-                            <h3 className="titleFavoriteHeader">
+                            <a href="/home" class="previous round">&larr;</a>
+                            <h3>
                                 All Caught
                             </h3>
                     </div>
@@ -66,63 +67,38 @@ function MyPokemons(){
                         </div>
                         <div className="AllCaughtCard">
                             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="pikachu Teste"/>
+                            <h4>Pikachu</h4>
+                            <h4>electry</h4>
                         </div>
                         <div className="AllCaughtCard">
                             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="pikachu Teste"/>
+                            <h4>Pikachu</h4>
+                            <h4>electry</h4>
                         </div>
                         <div className="AllCaughtCard">
                             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="pikachu Teste"/>
+                            <h4>Pikachu</h4>
+                            <h4>electry</h4>
                         </div>
                         <div className="AllCaughtCard">
                             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="pikachu Teste"/>
+                            <h4>Pikachu</h4>
+                            <h4>electry</h4>
                         </div>
                         <div className="AllCaughtCard">
                             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="pikachu Teste"/>
+                            <h4>Pikachu</h4>
+                            <h4>electry</h4>
                         </div>
                         <div className="AllCaughtCard">
                             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="pikachu Teste"/>
+                            <h4>Pikachu</h4>
+                            <h4>electry</h4>
                         </div>
-                        <div className="AllCaughtCard">
-                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="pikachu Teste"/>
-                        </div>
-                        <div className="AllCaughtCard">
-                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="pikachu Teste"/>
-                        </div>
-
-                        <div className="AllCaughtCard">
-                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="pikachu Teste"/>
-                        </div>
-                        <div className="AllCaughtCard">
-                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="pikachu Teste"/>
-                        </div>
-                        <div className="AllCaughtCard">
-                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="pikachu Teste"/>
-                        </div>
-                        <div className="AllCaughtCard">
-                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="pikachu Teste"/>
-                        </div>
-                        <div className="AllCaughtCard">
-                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="pikachu Teste"/>
-                        </div>
-                        <div className="AllCaughtCard">
-                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="pikachu Teste"/>
-                        </div>
-                        
-                        
-                        
-                        
-                        
-
                     </div>
-
-
-                    
-
                 </div>
             </div>
-            
         </div>
-
     </div>);
 }
 
