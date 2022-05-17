03ec021e9a6f8e4e570db22ac721d4ceabaf5734
leticiacb1 <72461834+leticiacb1@users.noreply.github.com>
2022-05-16 23:23:53 -0300

    Subindo tela de batalha com informacoes na tela.

--- pokemon-frontend/src/components/Battle/battle.js
+++ pokemon-frontend/src/components/Battle/battle.js
@@ -1,4 +1,10 @@
 import { useNavigate , useLocation } from "react-router-dom";
+import  React , { useState , useEffect} from "react";
+import Background from "../AnimatedBackground/backgroung.js";
+import './battle.css';
+import arena from './arena2.png'
+
+const axios = require("axios");
 
 function Battle(props){
 
@@ -9,12 +15,167 @@ function Battle(props){
     let username = location.state.username
     let enemy = location.state.enemy
 
-    console.log(battlePokemonName)
-    console.log(username)
-    console.log(enemy)
+    let [damage , setDamage] = useState(0)
+
+    // Meu pokemon :
+    const [srcImgBack, setsrcImgBack] = useState([''])
+    const [hp, setHp] = useState([''])
+    const [level, setLevel] = useState([''])
+    const [move1, setmove1] = useState([''])
+    const [move2, setmove2] = useState([''])
+    const [move3, setmove3] = useState([''])
+
+    // Inimigo :
+    const [srcImgEnemy, setsrcImgEnemy] = useState([''])
+    const [srcImgEnemyBack, setsrcImgEnemyBack] = useState([''])
+    const [typeEnemy, settypeEnemy] = useState([''])
+    const [move1Enemy, setmove1Enemy] = useState([''])
+    const [move2Enemy, setmove2Enemy] = useState([''])
+    const [move3Enemy, setmove3Enemy] = useState([''])
+    const [baseExperienceEnemy, setbaseExperienceEnemy] = useState([''])
+    const [hpEnemy, setHpEnemy] = useState(250)
+    const [levelEnemy, setLevelEnemy] = useState(5)
+
+    async function getUserPokemonInformation(){
+        
+        const url = 'http://localhost:8000/pokemons/'+username+'/'+battlePokemonName;
+
+        axios
+        .get(url)
+        .then((response) => {
+
+            setsrcImgBack(response.data.srcImgBack);
+            setHp(response.data.hp);
+            setLevel(response.data.level);
+            setmove1(response.data.move1);
+            setmove2(response.data.move2);
+            setmove3(response.data.move3);
+
+        }, (error) => console.log(error));
+    }
+
+    async function getEnemyPokemon(){
+        const url = 'https://pokeapi.co/api/v2/pokemon/'+enemy+'/'
+
+        axios
+            .get(url)
+            .then((response) => {
+                
+                if(response.status == 200){
+                    settypeEnemy(response.data.types[0].type.name);
+                    setsrcImgEnemy(response.data.sprites.front_default);
+                    setsrcImgEnemyBack(response.data.sprites.back_default);
+                    setmove1Enemy(response.data.moves[0].move.name);
+                    setmove2Enemy(response.data.moves[1].move.name);
+                    setmove3Enemy(response.data.moves[2].move.name);
+                    setbaseExperienceEnemy(response.data.abilities.base_experience)
+                }
+                
+            }, (error) => console.log(error));
+
+    }
+
+    function onChangeValue (event){
+        console.log(event.target.value)
+    }
+
+    useEffect(() => {
+        getEnemyPokemon()
+        getUserPokemonInformation()
+    }, [])
 
     return (
-        <h3> Batalha !</h3>
+        <>
+            <Background></Background>
+            <div className="backScreenBattle">
+                <div className="containerBattle">
+                    <div className="arena">
+                        
+                        <div className="enemyDiv">
+                            <div className="enemyContent">
+                                <div className="infoEnemyTitle"><h4> {enemy} </h4> </div>
+                                <div className="infoEnemy"><h4>Level:&ensp;</h4> {levelEnemy}</div>
+                                <div className="infoEnemy"><h4>Type:&ensp;</h4>{ typeEnemy}</div>
+                                <div className="infoEnemy"><h4>Experience:&ensp;</h4>{baseExperienceEnemy}</div>
+                                <div className="infoEnemy"><h4>HP:&ensp;</h4>{hpEnemy}</div>
+                            </div>
+
+                            <img src = {srcImgEnemy} alt="enemy" className="enemySprite"></img>
+                        </div>
+
+                        <div className="pokemonDiv">
+                            <img src={srcImgBack} alt="myPokemon" className="myPokemonSprite"></img>
+
+                            <div className="myPokemonContent">
+                                <div className="infoMyPokemonTitle"><h4> {battlePokemonName} </h4> </div>
+                                <div className="infoMyPokemon"><h4>Level :&ensp;</h4> {level}</div>
+                                <div className="infoMyPokemon"><h4>HP :&ensp;</h4>{hp}</div>
+                            </div>
+
+                        </div>
+                
+                    </div>
+
+                    <div className="movesPlayer">
+
+                            <div className="headerBattle">
+                                <div className="contentHeaderBattle">
+                                    <h3 className="battleTitle">Choose a attack: </h3>
+                                </div>  
+                            </div>
+
+                            <div className="movesOptions"  onChange={(event)=>onChangeValue(event)}>
+                                <div className="actionsCards">
+                                    <input type="radio" name="move1" value="move1" className="selectMove"></input>
+                                    <div className="moveInfo">
+                                        <h4 className="moveName">
+                                            {move1}
+                                        </h4>
+                                        <div className="descriptionMove">
+                                            <h4 className="informationMove">Accuracy : 10 %</h4>
+                                            <h4>Damage: Hight</h4>     
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="actionsCards">
+                                    <input type="radio" name="move1" value="move2" className="selectMove"></input>
+                                    <div className="moveInfo">
+                                        <h4 className="moveName">
+                                            {move2}
+                                        </h4>
+                                        <div className="descriptionMove">
+                                            <h4 className="informationMove">Accuracy : 50 %</h4>
+                                            <h4>Damage: Medium</h4>     
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="actionsCards">
+                                    <input type="radio" name="move1" value="move3" className="selectMove"></input>
+                                    <div className="moveInfo">
+                                        <h4 className="moveName">
+                                            {move3}
+                                        </h4>
+                                        <div className="descriptionMove">
+                                            <h4 className="informationMove">Accuracy : 80 %</h4>
+                                            <h4>Damage: Low</h4>     
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="attackButton">
+                                <button className="attack">
+                                    Attack !
+                                </button>
+                            </div>
+                        
+                    </div>
+                </div>
+                
+            </div>
+            
+        </>
+        
     );
 }
 
